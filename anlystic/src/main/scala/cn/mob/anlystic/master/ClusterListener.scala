de85bba9874b70b51e24b7bb58db2d1a1a9cfc9f
dempeZheng <752996510@qq.com>
2014-09-12 17:19:25 +0800

    add cluster support

--- anlystic/src/main/scala/cn/mob/anlystic/master/ClusterListener.scala
+++ anlystic/src/main/scala/cn/mob/anlystic/master/ClusterListener.scala
@@ -1,9 +1,37 @@
 package cn.mob.anlystic.master
 
+import akka.actor.{Actor, ActorLogging}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
+import akka.cluster.ClusterEvent.MemberUp
+import akka.cluster.ClusterEvent.UnreachableMember
+
 /**
  * @version 1.0 date : 2014/9/12
  * @author : Dempe 
  */
-class ClusterListener {
+class ClusterListener  extends Actor with ActorLogging {
+
+  val cluster = Cluster(context.system)
+
+  // subscribe to cluster changes, re-subscribe when restart
+  override def preStart(): Unit = {
+    //#subscribe
+    cluster.subscribe(self, initialStateMode = InitialStateAsEvents,
+      classOf[MemberEvent], classOf[UnreachableMember])
+    //#subscribe
+  }
+  override def postStop(): Unit = cluster.unsubscribe(self)
+
+  def receive = {
+    case MemberUp(member) =>
+      log.info("Member is Up: {}", member.address)
+    case UnreachableMember(member) =>
+      log.info("Member detected as unreachable: {}", member)
+    case MemberRemoved(member, previousStatus) =>
+      log.info("Member is Removed: {} after {}",
+        member.address, previousStatus)
+    case _: MemberEvent => // ignore
+  }
 
 }
