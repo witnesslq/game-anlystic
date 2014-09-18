52ff015aabd17bbd64395c28670815434d581e86
dempeZheng <752996510@qq.com>
2014-09-18 10:48:44 +0800

    update readme

--- node/src/main/java/cn/mob/node/Constants.java
+++ node/src/main/java/cn/mob/node/Constants.java
@@ -8,9 +8,12 @@ public class Constants {
 
     public static final String CONF_NAME = "node.properties";
     public static final String PRE_NODES = "pre.node";
-    public static final String NEXT_NODES = "next.node";
     public static final String LOCAL_NODE = "local.node";
     public static final String NODE_NAME = "node.server.name";
+
+    public static final String IDENTIFY = "identity";
+    public static final String JOB = "job";
+    public static final String USER = "user";
 
 
 }
